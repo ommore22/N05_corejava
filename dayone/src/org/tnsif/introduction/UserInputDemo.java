455939225f5bd433e76ae93437730f8bc685913d
Om More <Om More@LAPTOP-MM1Q5QGE>
2023-07-20 12:07:13 +0530

    First  commit

---
+++ dayone/src/org/tnsif/introduction/UserInputDemo.java
@@ -0,0 +1,25 @@
+//program to demonstrate on user input
+package org.tnsif.introduction;
+import java.util.Scanner;
+public class UserInputDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner s=new Scanner(System.in);
+		/*System.out.println("enter a number");
+		int num=s.nextInt();
+		System.out.println("the value of num is"+num);*/
+
+		
+		
+		/*System.out.println("enter a number");
+		float num1=s.nextInt();
+		System.out.println("the value of num is"+num1);*/
+		
+		//System.out.println("enter a number");
+		String str=s.nextLine();
+		//s.next();
+		System.out.println("string is : " +str);
+	}
+
+}
